300541dd553c7d34aac81427728905ebedb71f13
iremkpr <38679907+iremkoprek@users.noreply.github.com>
2024-08-13 23:56:22 -0400

    DataDriven

--- src/test/java/TestBase/PageInitializer.java
+++ src/test/java/TestBase/PageInitializer.java
@@ -4,6 +4,7 @@ import pages.AddEmployeePage;
 import pages.DashboardPage;
 import pages.EmployeeListPage;
 import pages.GooglePage;
+import pages.PersonalDetailsPage;
 import pages.loginPage;
 
 public class PageInitializer extends BaseClass{
@@ -13,7 +14,7 @@ public class PageInitializer extends BaseClass{
 	public static DashboardPage dashboard;
 	public static EmployeeListPage empListPage;
 	public static AddEmployeePage addEmp;
-	
+	public static PersonalDetailsPage personalDetails;
 	
 	public static void initialize() {
 		google=new GooglePage();
@@ -21,5 +22,6 @@ public class PageInitializer extends BaseClass{
 		dashboard=new DashboardPage();
 		empListPage=new EmployeeListPage();
 		addEmp=new AddEmployeePage();
+		personalDetails=new PersonalDetailsPage();
 	}
  }
